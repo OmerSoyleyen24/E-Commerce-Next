27da9c8b70e4f47412a79b07764f52c476ea88e6
Ömer Söyleyen <153733533+OmerSoyleyen24@users.noreply.github.com>
2025-07-31 21:01:11 +0300

    Update Card.tsx

--- frontend/component/Card/Card.tsx
+++ frontend/component/Card/Card.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import CardProgress from "../CardProgress";
-import CardTable from "../CardTable";
-import CardCoupon from "../CardCoupon";
-import CardTotals from "../CardTotals";
+import CardProgress from "./CardProgress";
+import CardTable from "./CardTable";
+import CardCoupon from "./CardCoupon";
+import CardTotals from "./CardTotals";
 import Policy from "@/component/Policy/Policy";
 import "./Card.css"
 
